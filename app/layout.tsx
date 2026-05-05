ee4806cf2108e312c907ec34eaf40aa5bc2fb66e
jantallus <97134165+jantallus@users.noreply.github.com>
2026-05-05 23:32:37 +0200

    h

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,12 @@ import localFont from 'next/font/local';
 
 const aeonik = localFont({
   src: [
-    { path: '../public/fonts/Aeonik-Light.woff2',        weight: '300', style: 'normal' },
-    { path: '../public/fonts/Aeonik-LightItalic.woff2',  weight: '300', style: 'italic' },
-    { path: '../public/fonts/Aeonik-Regular.woff2',      weight: '400', style: 'normal' },
-    { path: '../public/fonts/Aeonik-RegularItalic.woff2',weight: '400', style: 'italic' },
-    { path: '../public/fonts/Aeonik-Bold.woff2',         weight: '700', style: 'normal' },
-    { path: '../public/fonts/Aeonik-BoldItalic.woff2',   weight: '700', style: 'italic' },
+    { path: './fonts/Aeonik-Light.woff2',        weight: '300', style: 'normal' },
+    { path: './fonts/Aeonik-LightItalic.woff2',  weight: '300', style: 'italic' },
+    { path: './fonts/Aeonik-Regular.woff2',       weight: '400', style: 'normal' },
+    { path: './fonts/Aeonik-RegularItalic.woff2', weight: '400', style: 'italic' },
+    { path: './fonts/Aeonik-Bold.woff2',          weight: '700', style: 'normal' },
+    { path: './fonts/Aeonik-BoldItalic.woff2',    weight: '700', style: 'italic' },
   ],
   display: 'swap',
   variable: '--font-aeonik',
